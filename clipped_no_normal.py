45d765f61f2c7e567d09b8ea8276de06ad24b384
Jingjing Tang <31444565+jatangne@users.noreply.github.com>
2018-05-04 14:42:20 -0400

    Add files via upload
    MIAS-clipped-no normal

---
+++ clipped_no_normal.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  4 14:38:50 2018
+
+@author: jingjingtang
+"""
+
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt # showing and rendering figures
+# io related
+import os
+from skimage.io import imread, imread, imsave, imshow
+import h5py
+from scipy import misc
+from seam_carver import intelligent_resize
+import matplotlib.pyplot as plt
+from resizeimage import resizeimage
+from PIL import Image
+
+base_h5 = os.path.join('mias-mammography', 'all_mias_scans.h5')
+tif_dir = 'tiffs'
+os.makedirs(tif_dir, exist_ok=True)
+with h5py.File(base_h5, 'r') as f:
+    mammo_df = pd.DataFrame(
+        {k: v.value if len(v.shape)==1 else [sub_v for sub_v in v] 
+         for k,v in f.items()}
+    )
+for k in mammo_df.columns:
+    if isinstance(mammo_df[k].values[0], bytes):
+        mammo_df[k] = mammo_df[k].map(lambda x: x.decode())
+
+
+def to_path(c_row):
+    out_path = os.path.join(tif_dir, '%s.tif' % c_row['REFNUM'])
+    imsave(out_path, c_row['scan'])
+    return out_path
+mammo_df['scan'] = mammo_df.apply(to_path,1)
+mammo_df['path'] = 'mias-mammography/all-mias/' + mammo_df['path'].astype(str)
+mammo_df = mammo_df.dropna(axis=0, how='any')
+mammo_df['X'] = mammo_df['X'].astype(int)
+mammo_df['Y'] = mammo_df['Y'].astype(int)
+mammo_df['RADIUS'] = mammo_df['RADIUS'].astype(int)
+mammo_df = mammo_df.reset_index()
+
+
+# clipped version of the original images without normal
+exist = {}
+clipped_path = []
+for i in mammo_df.index:
+    image_dir = os.path.join(mammo_df['path'][i])
+    image = Image.open(image_dir)
+    if mammo_df['CLASS'][i] == 'NORM':
+        continue
+    else:
+        
+        image = image.crop(
+                [mammo_df['X'][i]-mammo_df['RADIUS'][i], 
+                 1024-mammo_df['Y'][i]-mammo_df['RADIUS'][i],
+                 mammo_df['X'][i]+mammo_df['RADIUS'][i],
+                 1024-mammo_df['Y'][i]+mammo_df['RADIUS'][i]]
+                )
+    if mammo_df['REFNUM'][i] in exist.keys():
+        exist[mammo_df['REFNUM'][i]] += 1
+        write_dir = os.path.join('clipped', mammo_df['REFNUM'][i] + '_%d_clipped.tiff' % exist[mammo_df['REFNUM'][i]])
+    else:
+        exist[mammo_df['REFNUM'][i]] = 1
+        write_dir = os.path.join('clipped', mammo_df['REFNUM'][i] + '_clipped.tiff')
+    image = image.resize([48, 48])
+    image.save(write_dir)
+    clipped_path.append(write_dir)
+    
+
+
+from sklearn.preprocessing import LabelEncoder
+from keras.utils.np_utils import to_categorical
+class_enc = LabelEncoder()
+mammo_df['CLASS_ID'] = class_enc.fit_transform(mammo_df['CLASS'])
+mammo_df['CLASS_VEC'] = mammo_df['CLASS_ID'].map(lambda x: to_categorical(x, 
+                                                                          num_classes=len(class_enc.classes_)))
+severity_enc = LabelEncoder()
+mammo_df['SEVERITY_ID'] = severity_enc.fit_transform(mammo_df['SEVERITY'])
+
+        
+
+mammo_df[['CLASS_ID', 'RADIUS', 'SEVERITY']].hist(figsize = (10, 5))
+ 
+
+   
+from sklearn.model_selection import train_test_split
+raw_train_df, valid_df = train_test_split(mammo_df, 
+                                   test_size = 0.25, 
+                                   random_state = 2018,
+                                   stratify = mammo_df[['CLASS_ID', 'SEVERITY']])
+print('train', raw_train_df.shape[0], 'validation', valid_df.shape[0])
+raw_train_df.sample(1)
+
+
+
+train_df = raw_train_df.groupby(['CLASS', 'SEVERITY']).apply(lambda x: x.sample(100, replace = True)
+                                                      ).reset_index(drop = True)
+print('New Data Size:', train_df.shape[0], 'Old Size:', raw_train_df.shape[0])
+train_df[['SEVERITY_ID', 'RADIUS']].hist(figsize = (10, 5))
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.imagenet_utils import preprocess_input
+IMG_SIZE = (192, 192) # slightly smaller than vgg16 normally expects
+core_idg = ImageDataGenerator(samplewise_center=False, 
+                              samplewise_std_normalization=False, 
+                              horizontal_flip = True, 
+                              vertical_flip = False, 
+                              height_shift_range = 0.15, 
+                              width_shift_range = 0.15, 
+                              rotation_range = 5, 
+                              shear_range = 0.01,
+                              fill_mode = 'nearest',
+                              zoom_range=0.2,
+                             preprocessing_function = preprocess_input)
+
+def flow_from_dataframe(img_data_gen, in_df, path_col, y_col, **dflow_args):
+    base_dir = os.path.dirname(in_df[path_col].values[0])
+    print('## Ignore next message from keras, values are replaced anyways')
+    df_gen = img_data_gen.flow_from_directory(base_dir, 
+                                     class_mode = 'sparse',
+                                    **dflow_args)
+    df_gen.filenames = in_df[path_col].values
+    df_gen.classes = np.stack(in_df[y_col].values)
+    df_gen.samples = in_df.shape[0]
+    df_gen.n = in_df.shape[0]
+    df_gen._set_index_array()
+    df_gen.directory = '' # since we have the full path
+    print('Reinserting dataframe: {} images'.format(in_df.shape[0]))
+    return df_gen
+
+
+raw_train_gen = flow_from_dataframe(core_idg, raw_train_df, 
+                             path_col = 'clipped_path',
+                            y_col = 'SEVERITY_ID', 
+                            target_size = IMG_SIZE,
+                             color_mode = 'rgb',
+                            batch_size = 32)
+
+train_gen = flow_from_dataframe(core_idg, train_df, 
+                             path_col = 'clipped_path',
+                            y_col = 'SEVERITY_ID', 
+                            target_size = IMG_SIZE,
+                             color_mode = 'rgb',
+                            batch_size = 32)
+
+valid_gen = flow_from_dataframe(core_idg, valid_df, 
+                             path_col = 'clipped_path',
+                            y_col = 'SEVERITY_ID', 
+                            target_size = IMG_SIZE,
+                             color_mode = 'rgb',
+                            batch_size = 256) # we can use much larger batches for evaluation
+# used a fixed dataset for evaluating the algorithm
+test_X, test_Y = next(flow_from_dataframe(core_idg, 
+                               valid_df, 
+                             path_col = 'clipped_path',
+                            y_col = 'SEVERITY_ID', 
+                            target_size = IMG_SIZE,
+                             color_mode = 'rgb',
+                            batch_size = 2048)) # one big batch
+
+
+from keras.applications.vgg16 import VGG16
+from keras.layers import GlobalAveragePooling2D, Dense, Dropout, Flatten, Input, Conv2D, multiply, LocallyConnected2D, Lambda
+from keras.models import Model
+in_lay = Input(t_x.shape[1:])
+base_pretrained_model = VGG16(input_shape =  t_x.shape[1:], include_top = False, weights = 'imagenet')
+base_pretrained_model.trainable = False
+pt_depth = base_pretrained_model.get_output_shape_at(0)[-1]
+pt_features = base_pretrained_model(in_lay)
+from keras.layers import BatchNormalization
+bn_features = BatchNormalization()(pt_features)
+
+# here we do an attention mechanism to turn pixels in the GAP on an off
+
+attn_layer = Conv2D(64, kernel_size = (1,1), padding = 'same', activation = 'relu')(bn_features)
+attn_layer = Conv2D(16, kernel_size = (1,1), padding = 'same', activation = 'relu')(attn_layer)
+attn_layer = Conv2D(1, 
+                                kernel_size = (1,1), 
+                                padding = 'valid', 
+                                activation = 'sigmoid')(attn_layer)
+# fan it out to all of the channels
+up_c2_w = np.ones((1, 1, 1, pt_depth))
+up_c2 = Conv2D(pt_depth, kernel_size = (1,1), padding = 'same', 
+               activation = 'linear', use_bias = False, weights = [up_c2_w])
+up_c2.trainable = False
+attn_layer = up_c2(attn_layer)
+
+mask_features = multiply([attn_layer, bn_features])
+gap_features = GlobalAveragePooling2D()(mask_features)
+gap_mask = GlobalAveragePooling2D()(attn_layer)
+# to account for missing values from the attention model
+gap = Lambda(lambda x: x[0]/x[1], name = 'RescaleGAP')([gap_features, gap_mask])
+gap_dr = Dropout(0.5)(gap)
+dr_steps = Dropout(0.25)(Dense(128, activation = 'elu')(gap_dr))
+out_layer = Dense(1, activation = 'sigmoid')(dr_steps) # linear is what 16bit did
+mammo_model = Model(inputs = [in_lay], outputs = [out_layer])
+
+mammo_model.compile(optimizer = 'adam', loss = 'binary_crossentropy',
+                           metrics = ['binary_accuracy'])
+
+mammo_model.summary()
+
+
+
+t_x, t_y = next(train_gen)
+fig, m_axs = plt.subplots(2, 4, figsize = (16, 8))
+for (c_x, c_y, c_ax) in zip(t_x, t_y, m_axs.flatten()):
+    c_ax.imshow(c_x[:,:,0], cmap = 'bone', vmin = -127, vmax = 127)
+    c_ax.set_title('%s' % (class_enc.classes_[np.argmax(c_y,-1)]))
+    c_ax.axis('off')
+    
+    
+    
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, EarlyStopping, ReduceLROnPlateau
+weight_path="{}_weights.best.hdf5".format('mammo_result')
+
+checkpoint = ModelCheckpoint(weight_path, monitor='val_loss', verbose=1, 
+                             save_best_only=True, mode='min', save_weights_only = True)
+
+
+reduceLROnPlat = ReduceLROnPlateau(monitor='val_loss', factor=0.8, patience=10, verbose=1, mode='auto', min_delta=0.0001, cooldown=5, min_lr=0.0001)
+early = EarlyStopping(monitor="val_loss", 
+                      mode="min", 
+                      patience=5) # probably needs to be more patient, but kaggle time is limited
+callbacks_list = [checkpoint, early, reduceLROnPlat]
+
+
+mammo_model.fit_generator(train_gen, 
+                                  steps_per_epoch = 35,
+                                  validation_data = (test_X, test_Y), 
+                                  epochs = 5, 
+                                  callbacks = callbacks_list)
+
+
+
